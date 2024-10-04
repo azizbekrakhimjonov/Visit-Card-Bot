4c3ee82bdaae74d1c68c606382dffd640da3039d
azizbekrakhimjonov <azizbekrahimjonov571@gmail.com>
2024-10-04 16:30:44 +0500

    edit btn

--- buttons.py
+++ buttons.py
@@ -1,5 +1,6 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
+# buttons
 cancel_button = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Cancel"))
 start_button = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("/start"))
 result = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton("Get Visit Card"))
